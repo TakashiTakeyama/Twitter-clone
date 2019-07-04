1b154db54b6b46d6998f81ee6cd74a7b79f99b2c
Takashi <Takashi@gmail.com>
2019-07-04 17:12:16 +0900

    編集を機能を作成

--- app/controllers/tubuyakis_controller.rb
+++ app/controllers/tubuyakis_controller.rb
@@ -1,4 +1,5 @@
 class TubuyakisController < ApplicationController
+  before_action :set_tubuyaki, only: [:edit,:destroy,:update]
 
   def index
     @tubuyaki = Tubuyaki.all
@@ -17,6 +18,18 @@ class TubuyakisController < ApplicationController
     end
   end
 
+  def edit
+    @tubuyaki = Tubuyaki.find(params[:id])
+  end
+
+  def update
+    if @tubuyaki.update(tubuyaki_params)
+      redirect_to tubuyakis_path, notice: "つぶやきを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @tubuyaki = Tubuyaki.find(params[:id])
     @tubuyaki.destroy
@@ -27,6 +40,10 @@ class TubuyakisController < ApplicationController
 
   def tubuyaki_params
     params.require(:tubuyaki).permit(:content)
+  end
+
+  def set_tubuyaki
+    @tubuyaki = Tubuyaki.find(params[:id])
   end
 
 end
